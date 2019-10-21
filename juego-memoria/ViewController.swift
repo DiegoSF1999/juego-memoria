0e2782ca80e67312eae17bbbf5da3b0e39803896
L.P <lorenzo_perez_1ta1819@cev.com>
2019-10-21 09:06:52 +0200

    ya queda la mitad o menos

--- juego-memoria/ViewController.swift
+++ juego-memoria/ViewController.swift
@@ -13,18 +13,22 @@ class ViewController: UIViewController {
     
 // Outlets
     
+    
 
-    @IBOutlet weak var Touch: UIStackView!
+    @IBOutlet weak var Label: UILabel!
+    @IBOutlet weak var Imageview: UIImageView!
+    let intro_image = #imageLiteral(resourceName: "memory")
+    let images = [#imageLiteral(resourceName: "pyke"), #imageLiteral(resourceName: "kata"), #imageLiteral(resourceName: "garen"), #imageLiteral(resourceName: "ashe")]
     
     // Codigo
     override func viewDidLoad() {
         super.viewDidLoad()
         
-Touch.isHidden = true
+     
+    Start()
+    
         
-        
-      
-
+    
     
         
         
@@ -33,5 +37,36 @@ Touch.isHidden = true
     
 
     }
+    
+    
+    func Start() {
+        Imageview.image = intro_image
+        Label.text = ""
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.Show()
+        }
+       
+    }
+    
+    func Show() {
+        var suffled_images = images
+        suffled_images.shuffle()
+        
+        let Count: Int = suffled_images.count
+        
+        //for i in 0...Count-1 {
+            
+            Label.text = String(1)
+            
+            Imageview.image = suffled_images[0]
+            
+            sleep(4)
+            
+            
+        //}
+        
+        
+    }
+    
 }
 
